f8189094859f6e28f0136a4f5f82c8665b927c2a
Corsair909 <corsar909@gmail.com>
2017-09-29 00:23:08 +0300

    Add full functionality

---
+++ routes/index.js
@@ -0,0 +1,49 @@
+var moviesJSON = require('../movies.json');
+
+//home
+exports.home = function(req, res) {
+
+    var movies = moviesJSON.movies;
+
+    res.render('home', {
+        title: "Звёздные войны",
+        movies: movies
+    });
+};
+
+// movie single
+exports.movie_single = function(req, res) {
+    var episode_number = req.params.episode_number;
+
+    var movies = moviesJSON.movies;
+
+    // Only render the movie_single template for episodes that exist
+    if (episode_number >= 1 && episode_number <= 6) {
+        var movie = movies[episode_number - 1];
+
+        var title = movie.title;
+
+        var main_characters = movie.main_characters;
+
+        res.render('movie_single', {
+            movies : movies,
+            movie : movie,
+            title : title,
+            main_characters : main_characters
+        });
+    } else {
+        res.render('notFound', {
+            movies : movies,
+            title: "Ой, этой страницы нет"
+        });
+    }
+};
+
+// notFound
+exports.notFound = function(req, res) {
+    var movies = moviesJSON.movies;
+    res.render('notFound', {
+        movies : movies,
+        title : "Ой, эта страница отсутствует"
+    });
+};
